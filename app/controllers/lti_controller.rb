771bc19ee832c670375eef514d8af49926856a71
Jenny Allar <JennyAllar@gmail.com>
2014-08-10 21:32:54 -0600

    Altered oauth using lti gem built in functionality.

--- app/controllers/lti_controller.rb
+++ app/controllers/lti_controller.rb
@@ -1,14 +1,12 @@
+require 'ims/lti'
+require 'oauth/request_proxy/rack_request'
+
 
 class LtiController < ApplicationController
 
-
-  $oauth_creds = {"test" => "secret", "testing" => "supersecret"}
-
-  CONSUMER_KEY = "jisc.ac.uk"
-  CONSUMER_SECRET = "secret"
+  $oauth_creds = {"ginkgo" => "secret", "tree" => "supersecret"}
 
   def index
-
     params.each do |k, v|
       if k.include?("oauth")
         Rails.logger.info "#{k} => #{v}"
@@ -21,30 +19,16 @@ class LtiController < ApplicationController
       if secret = $oauth_creds[key]
         @tp = IMS::LTI::ToolProvider.new(key, secret, params)
       else
+        render text: "Invalid Request"
         @tp = IMS::LTI::ToolProvider.new(nil, nil, params)
-        @tp.lti_msg = "Your consumer didn't use a recognized key."
-        @tp.lti_errorlog = "You did it wrong!"
         return false
       end
     else
-      "No consumer key"
       return false
     end
+    
+    @username = @tp.username("User")
 
-    if correct?
-      render index
-    else
-      render text: "Invalid Request"
-    end
   end
 
-  # TODO: do crazy oauth stuff to verify request
-
-  def correct?
-    if params[:oauth_consumer_key] == CONSUMER_KEY
-      true
-    else
-      false
-    end
-  end
 end
